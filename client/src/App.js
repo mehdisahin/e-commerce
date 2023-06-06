848566bfc59dae212584194f63dc07f4048138b8
Mehdi Şahin <sahinmehdi23@gmail.com>
2023-06-06 21:15:44 +0300

    add router

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <span>Home</span>,
+  },
+  {
+    path: "/products/:id",
+    element: <span>Category</span>,
+  },
+  {
+    path: "/product/:id",
+    element: <span>Product</span>,
+  },
+]);
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+export default App;
