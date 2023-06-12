7d7e3def8a576203b9adc140e59e5905ca0cc954
Mehdi Şahin <sahinmehdi23@gmail.com>
2023-06-12 16:02:36 +0300

    add mui and icons

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left"></div>
+        <div className="center"></div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
+};
